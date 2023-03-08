17071b5e074a0ba6e74f12a966aa56572d0f01ee
ikarusbuszok <nagybene7@gmail.com>
2023-03-08 11:23:40 +0100

    kesz

---
+++ script.js
@@ -0,0 +1,31 @@
+window.addEventListener("load", init);
+function init() {
+    const articleELEM = document.querySelectorAll("article");
+    articleELEM[0].innerHTML="<div>img src='kepek/ikarus-260-bal-eleje-45fokban-normal.jpg' 'alt=képem'></div>";
+    var button = document.getElementById("myButton");
+    const gombELEM = document.querySelector("article div");
+    gombELEM.addEventListener("click", function() {
+        console.log("Kattintottál");
+        kepELEM.addEventListener("mouseover", function() {
+
+        });
+
+
+        kepELEM.classList.add("kepformazas")
+
+
+        kepELEM.addEventListener ("mouseout", function() {
+
+
+        });
+
+
+
+
+
+
+
+    
+}
+        
+    
